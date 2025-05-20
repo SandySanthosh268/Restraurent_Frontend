81eb92d36592676f34b8eb8a2c1992c20881f363
NARSI NANDHU <santhoshayyanar268.com>
2025-05-20 08:14:21 +0530

    first commit

---
+++ backend/database/dbConnection.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () =>{
+    mongoose.connect(process.env.MONGO_URL, {
+        dbName: "RESTAURANT"
+    }).then(()=> {
+        console.log("CONNECTED TO DATABASE sUCCESSFULLY!");
+        
+    }).catch(err=>{
+        console.log("some error occured while connecting to database");
+        
+    })
+}
